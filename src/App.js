feb68be5e6dbc3b0ae130e1f6ecc785a0259305a
Gautier Borot <g.borot@gmail.com>
2019-12-10 16:49:16 +0100

    added a method onClick with swithNameHandler class method

--- src/App.js
+++ src/App.js
@@ -11,11 +11,15 @@ class App extends React.Component {
     ]
   };
 
+  switchNameHandler = () => {
+    console.log("Was clicked");
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hello i'm Gautier !</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
